a5453acb1d69ac570a7ad20f9e075ce05b07bd83
Mark <mlong@mindspring.com>
2016-12-17 14:09:48 -0500

    start with tests

---
+++ UnitTestRoman/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestRoman
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Assert_1_is_I()
+        {
+        }
+        [TestMethod]
+        public void Assert_I_is_1()
+        {
+        }
+    }
+}
